98ad4dcfe91b6e094c360e565cfaebbd6526ece8
robert <robert>
2013-12-25 09:33:52 -0500

    change content type

--- src/main/java/test/HelloWorld.java
+++ src/main/java/test/HelloWorld.java
@@ -9,6 +9,7 @@ public class HelloWorld implements SparkApplication {
 		Spark.get(new Route("/hello") {
 		 @Override
 		 public Object handle(Request request, Response response) {
+			response.header("Content-Type", "text/html; charset=utf-8");
 			return "Hello World!";
 		 }
 		});
